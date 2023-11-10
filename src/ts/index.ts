f40462109f09670d7bf1c7f75324854f6b61045a
Hero Protagonist <heroprotagonist32@gmail.com>
2023-11-09 21:50:20 -0300

    debugger options for ts>js

---
+++ src/ts/index.ts
@@ -0,0 +1,146 @@
+class Library{
+    storage: Book[]
+
+    constructor(){
+        this.storage = []
+    }
+}
+
+class Book{
+
+    title: string;
+    author : string;
+    read : boolean
+    pages? : number;
+    genre? : string[] | undefined;
+    readDate? : Date | undefined;
+    isbn?: string | undefined
+    
+    constructor(title :string , author : string, read : boolean, pages? : number, genre? : string[] , readDate? : Date, isbn?: string ){
+        this.title = title
+        this.author = author,
+        this.read = read;
+        this.pages = pages;
+        this.genre = genre,
+        this.readDate = readDate,
+        this.isbn = isbn;
+    }
+}
+const harryPotter = new Book('Harry el Potter', 'La muchacha', true)
+
+const mainLibrary = new Library,
+      sideLibrary = new Library;
+
+      mainLibrary.storage.push(harryPotter)
+
+      console.log(sideLibrary)
+      console.log(mainLibrary)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const bookGenres = [
+    "Mystery",
+    "Thriller",
+    "Science Fiction",
+    "Fantasy",
+    "Romance",
+    "Historical Fiction",
+    "Biography",
+    "Autobiography",
+    "Memoir",
+    "Self-Help",
+    "Travel",
+    "Science",
+    "Dystopian",
+    "Adventure",
+    "Children's Literature",
+    "Young Adult",
+    "Classic",
+    "Poetry",
+    "Drama",
+    "Comedy",
+    "Satire",
+    "Crime",
+    "Suspense",
+    "Detective",
+    "Espionage",
+    "Legal",
+    "Psychological",
+    "Gothic",
+    "Paranormal",
+    "Space Opera",
+    "Cyberpunk",
+    "Alternate History",
+    "Time Travel",
+    "High Fantasy",
+    "Urban Fantasy",
+    "Epic Fantasy",
+    "Historical Fantasy",
+    "Magical Realism",
+    "Fairy Tales",
+    "Mythology",
+    "Philosophy",
+    "Religion",
+    "Spirituality",
+    "Cookbooks",
+    "Essays",
+    "History",
+    "Science",
+    "Psychology",
+    "Sociology",
+    "Politics",
+    "Economics",
+    "True Crime",
+    "Sports",
+    "Art",
+    "Music",
+    "Film",
+    "Fashion",
+    "Food and Drink",
+    "Technology",
+    "Computer Science",
+    "Mathematics",
+    "Astronomy",
+    "Biology",
+    "Chemistry",
+    "Physics",
+    "Environmental Science",
+    "Cultural Studies",
+    "Literary Theory",
+    "Gender Studies",
+    "Race and Ethnicity",
+    "LGBT+ Studies",
+    "Political Science",
+    "Autobiography",
+    "Biography",
+    "Memoir",
+    "Letters",
+    "Diaries",
+    "Essay",
+    "Speech",
+    "Lecture",
+    "Sermon",
+    "Journalism",
+    "Reference",
+    "Encyclopedia",
+    "Dictionaries",
+  ];
+  
