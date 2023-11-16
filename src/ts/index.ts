262d0e86080e1f4a45e9a5cf3db47d2bbf467d7c
Hero Protagonist <heroprotagonist32@gmail.com>
2023-11-16 12:06:17 -0300

    showing results input input

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,6 @@
 import Tagify from './modules/tagify/dist/tagify'
 
+
 localStorage.clear()
 class Library{
     public storage : Book[]
@@ -55,6 +56,7 @@ class House extends Array<Library>{
 
 class Book{
     public id;
+    public value;
     constructor(
         public title :string , 
         public author : string, 
@@ -64,6 +66,7 @@ class Book{
         public img? : URL, 
         public isbn?: string ){
             this.id = this.setId()
+            this.value = this.title
         }
 
     private setId = () => {
@@ -135,10 +138,13 @@ for(const libraries of house){
 }
 
 // DOM declarations
+const body = document.querySelector('body')
 const main = document.querySelector('main');
         main?.addEventListener('mousedown', () => { 
             const newBookForm = document.getElementById('new-book-form')
+            const tagifyDropdown = document.querySelector('.tagify__dropdown')
             if(newBookForm) main.removeChild(newBookForm);
+            if(tagifyDropdown) body?.removeChild(tagifyDropdown);
         })
 
 const addBookTemplate = document.getElementById('new-book-template') as HTMLTemplateElement
@@ -153,10 +159,61 @@ const addBookBtn = document.getElementById('add-book')
             const addBookForm = document.getElementById('new-book-form')
             addBookForm?.addEventListener('mousedown',(ev)=> ev.stopPropagation())
 
-            const select = document.getElementById('genres')
-            if(select) new Tagify(select, {})
+            const select = document.getElementById('genres') as HTMLInputElement
+            if(select) new Tagify(select, {
+                whitelist: bookGenres,
+                dropdown : tagifyDropdown
+            })
 
-            ground?.classList.add('opaque')
+
+            const title = document.getElementById('title') as HTMLInputElement
+            let titleSelect:any;  // idk why i couldn't import types and 
+            if(title){
+                titleSelect = new Tagify(title,{
+                    enforceWhitelist: true,
+                    mode: "select",
+                    whitelist: [""],
+                    dropdown : tagifyDropdown,
+
+                    callbacks: {
+                        "input": (e:any) => {
+                            if(e.detail.value.length > 1){
+
+                                titleSelect.loading(true).dropdown.hide()
+
+                                const searchResults = googleGET(e.detail.value)
+                                searchResults.then((r)=>r.json())
+                                             .then((data)=>data)
+                                             .then((o)=>o.items)
+                                             .then((books)=>{
+                                                return books.map((b:any)=>b.volumeInfo)
+                                             })
+                                             .then((volumes:[])=>{
+                                                return volumes.map((v:any)=>{
+                                                    return new Book(
+                                                        v.title,
+                                                        (v.authors) ? v.authors.join(', ') : 'unknown',
+                                                        false,
+                                                        v.pageCount,
+                                                        v.categories,
+                                                        (v.imageLinks?.thumbnail) ? v.imageLinks.thumbnail : 'assets/placeholder.png'
+                                                        )
+                                                })
+                                             }).then((reformated)=>{
+                                                titleSelect.whitelist = reformated
+
+                                             }).then((v)=>{
+                                                titleSelect.loading(false).dropdown.show(v)
+                                             })
+                                             
+                                             
+                            }
+
+                        }
+                    }
+                    
+                })
+            }
         })
 
 
@@ -170,17 +227,6 @@ const harry = new Book('harry el potter','la señora', true, undefined , undefin
 
 
 
-
-async function digestPassword(password:string) { // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
-
-  const msgUint8 = new TextEncoder().encode(password);                      // encode as (utf-8) Uint8Array
-  const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8);       // hash the message
-  const hashArray = Array.from(new Uint8Array(hashBuffer));                 // convert buffer to byte array
-  const hashHex = hashArray
-                            .map((b) => b.toString(16).padStart(2, "0"))
-                            .join("");                                      // convert bytes to hex string
-  return hashHex;
-}
 
 
 // *** test's ***
@@ -214,120 +260,59 @@ const encodedHouse = encodeURIComponent(JSON.stringify(house)) // url
 
 /** Functions **/
 
-
-
-
-
-
-
-
-      
-
-
+// google api calls
 
 const googleApiKey = 'AIzaSyBbLoGrfBpVZrXlPHSeFkLniUZzG0o8NI8'
 const test_isbn = '9789568268992'
 const inputText = 'Lezama+Lima+Paradiso'
 
 
-
-async function testi(){
+async function googleGET(input:string){
 
     // const test_via_isbn = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${test_isbn}&key=${googleApiKey}`,{
     //     method: 'GET'
     // })
 
-    const test_search = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${inputText}&key=${googleApiKey}`,{
+    const general_search = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${input}&key=${googleApiKey}&langRestrict=en`,{
         method: 'GET'
     })
 
-    // fs.writeFileSync('/media/martincito/freedom/repos/minimalist-library/tmp/_response.json', JSON.stringify(await test_search.json()))
+    return general_search
+
 }
 
-const bookGenres = [
-    "Mystery",
-    "Thriller",
-    "Science Fiction",
-    "Fantasy",
-    "Romance",
-    "Historical Fiction",
-    "Biography",
-    "Autobiography",
-    "Memoir",
-    "Self-Help",
-    "Travel",
-    "Science",
-    "Dystopian",
-    "Adventure",
-    "Children's Literature",
-    "Young Adult",
-    "Classic",
-    "Poetry",
-    "Drama",
-    "Comedy",
-    "Satire",
-    "Crime",
-    "Suspense",
-    "Detective",
-    "Espionage",
-    "Legal",
-    "Psychological",
-    "Gothic",
-    "Paranormal",
-    "Space Opera",
-    "Cyberpunk",
-    "Alternate History",
-    "Time Travel",
-    "High Fantasy",
-    "Urban Fantasy",
-    "Epic Fantasy",
-    "Historical Fantasy",
-    "Magical Realism",
-    "Fairy Tales",
-    "Mythology",
-    "Philosophy",
-    "Religion",
-    "Spirituality",
-    "Cookbooks",
-    "Essays",
-    "History",
-    "Science",
-    "Psychology",
-    "Sociology",
-    "Politics",
-    "Economics",
-    "True Crime",
-    "Sports",
-    "Art",
-    "Music",
-    "Film",
-    "Fashion",
-    "Food and Drink",
-    "Technology",
-    "Computer Science",
-    "Mathematics",
-    "Astronomy",
-    "Biology",
-    "Chemistry",
-    "Physics",
-    "Environmental Science",
-    "Cultural Studies",
-    "Literary Theory",
-    "Gender Studies",
-    "Race and Ethnicity",
-    "LGBT+ Studies",
-    "Political Science",
-    "Autobiography",
-    "Biography",
-    "Memoir",
-    "Letters",
-    "Diaries",
-    "Essay",
-    "Speech",
-    "Lecture",
-    "Sermon",
-    "Journalism",
-    "Reference",
-    "Encyclopedia",
-    "Dictionaries",
-  ];
+
+
+
+// tagify options
+
+
+const tagifyDropdown = {
+    classname       : "color-blue",
+    enforceWhitelist: true,
+    enabled         : 2,              // show the dropdown immediately on focus
+    maxItems        : 5,
+    closeOnSelect   : true,          // keep the dropdown open after selecting a suggestion
+    searchKeys      : ["title", "author"]
+}
+
+
+
+      
+
+
+async function digestPassword(password:string) { // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
+
+    const msgUint8 = new TextEncoder().encode(password);                      // encode as (utf-8) Uint8Array
+    const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8);       // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer));                 // convert buffer to byte array
+    const hashHex = hashArray
+                              .map((b) => b.toString(16).padStart(2, "0"))
+                              .join("");                                      // convert bytes to hex string
+    return hashHex;
+  }
+
+
+
+const bookGenres = ["Mystery","Thriller","Science Fiction","Fantasy","Romance","Historical Fiction","Biography","Autobiography","Memoir","Self-Help","Travel","Science","Dystopian","Adventure","Children's Literature","Young Adult","Classic","Poetry","Drama","Comedy","Satire","Crime","Suspense","Detective","Espionage","Legal","Psychological","Gothic","Paranormal","Horror","Space Opera","Cyberpunk","Alternate History","Time Travel","High Fantasy","Urban Fantasy","Epic Fantasy","Historical Fantasy","Magical Realism","Fairy Tales","Mythology","Philosophy","Religion","Spirituality","Cookbooks","Essays","History","Psychology","Sociology","Politics","Economics","True Crime","Sports","Art","Music","Film","Fashion","Food and Drink","Technology","Computer Science","Mathematics","Astronomy","Biology","Chemistry","Physics","Environmental Science","Cultural Studies","Literary Theory","Gender Studies","Race and Ethnicity","LGBT+ Studies","Political Science","Autobiography","Biography","Memoir","Letters","Diaries","Essay","Speech","Lecture","Sermon","Journalism","Reference","Encyclopedia","Dictionaries"];
+
